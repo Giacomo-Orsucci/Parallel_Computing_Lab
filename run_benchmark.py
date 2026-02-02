aa7556e419b9af6761e278e882305579b4d68a26
Giacomo Orsucci <orsucci.giacomo@gmail.com>
2026-02-02 19:05:29 +0100

    Code instrumentations to test the sequential version.

---
+++ run_benchmark.py
@@ -0,0 +1,51 @@
+import os
+import subprocess
+
+
+#Script to execute parallel versions we want to test.
+
+EXECUTABLES = {
+    "Seq": "./cmake-build-benchmark/Seq",
+}
+
+cell_size = 2
+threads_values = [1]#, 2, 4, 8] #
+frames = 15000#
+#I'm interested in quadratic sizes, but the code is written to handle also other cases.
+screen_widths = [200]#400,800,1000,1200]#
+screen_heights = [200]#400,800,1000,1200]
+scan_size = 3 #3 is the standard for game of life
+n_experiments = 1 #
+CSV_OUT = "SEQ.csv" #
+gui = False
+
+def run_benchmarks(exe, width, height, frames, n_threads, csv, cell_size, scan_size, gui):
+
+    env = os.environ.copy()
+
+    cmd = [
+        exe,
+        "--width", str(width),
+        "--height", str(width),
+        "--frames", str(frames),
+        "--threads", str(n_threads),
+        "--csv", str(csv),
+        "--cell_size", str(cell_size),
+        "--scan_size", str(scan_size),
+        "--gui", str(gui)
+    ]
+
+
+    subprocess.run(cmd, env=env, capture_output=True, text=True)
+
+def main():
+
+    with open(CSV_OUT, mode="w", newline="") as f:
+        for layout, exe in EXECUTABLES.items():
+            for width, height in zip(screen_widths, screen_heights):
+                for n_threads in threads_values if layout != "Sequential" else [1]:
+                    for run_id in range(n_experiments):
+                        run_benchmarks(exe, width, height, frames, n_threads, CSV_OUT, cell_size, scan_size, gui)
+
+if __name__ == "__main__":
+    main()
--- seq_header.h
+++ seq_header.h
@@ -9,22 +9,70 @@
 #include <SFML/Graphics.hpp>
 #include <vector>
 #include <cstdlib>
+#include <string>
+#include <bits/stdc++.h>
 
 
-const int CELL_SIZE = 5;
-const int SCREEN_WIDTH = 800;
-const int SCREEN_HEIGHT = 600;
-const int SCAN_SIZE = 3;
+
+struct Config {
+
+    // this is the parameter to define the cell dimension. The cell dimension is equal
+    // to CELL_SIZExCELL_SIZE. If CELL_SIZE is set to 1, the cell is graphically
+    // invisible.
+
+    int CELL_SIZE = 2;
+
+    int SCREEN_WIDTH = 200;
+    int SCREEN_HEIGHT = 200;
+    int SCAN_SIZE = 3;
+    int FRAMES = 1000;
+    int THREADS = 1;
+
+    bool GUI = false;
+
+    std::string CSV;
 
 
-const int COLS = SCREEN_WIDTH / CELL_SIZE;
-const int ROWS = SCREEN_HEIGHT / CELL_SIZE;
+    //Parsing params passed via command line
+    void parse(int argc, char* argv[]) {
+        for (int i = 1; i < argc; ++i) { //i = 1 because for i=0 we always have the exe path
+            std::string arg = argv[i];
+            if (arg == "--width" && i + 1 < argc) {
+                SCREEN_WIDTH = std::stoi(argv[++i]);
+            } else if (arg == "--height" && i + 1 < argc) {
+                SCREEN_HEIGHT = std::stoi(argv[++i]);
+            } else if (arg == "--frames" && i + 1 < argc) {
+                FRAMES = std::stoi(argv[++i]);
+            }else if (arg == "--threads" && i + 1 < argc) {
+                THREADS = std::stoi(argv[++i]);
+            }else if (arg == "--csv" && i + 1 < argc) {
+                CSV = argv[++i];
+            }else if (arg == "--cell_size" && i + 1 < argc) {
+                CELL_SIZE = std::stoi(argv[++i]);
+            }else if (arg == "--scan_size" && i + 1 < argc) {
+                SCAN_SIZE = std::stoi(argv[++i]);
+            }else if (arg == "--gui" && i + 1 < argc) {
+                std::string GUI_app = argv[++i];
+                if (!GUI_app.compare("True"))
+                    GUI = true;
+                else
+                    GUI = false;
+                std::cerr << "GUI: " << GUI << std::endl;
+            }
+            else {
+                std::cerr << "Unknown argument: " << arg << std::endl;
+            }
+        }
+    }
+    int COLS = SCREEN_WIDTH / CELL_SIZE;
+    int ROWS = SCREEN_HEIGHT / CELL_SIZE;
+};
 
-inline int get_idx(int row, int col) {
+inline int get_idx(int row, int col, const int COLS) {
     return row * COLS + col;
 }
 
-void init_grid(std::vector<int>& grid) {
+void init_grid(std::vector<int>& grid, const int ROWS, const int COLS) {
 
     for (int i = 0; i < ROWS * COLS; ++i) {
         //to have a 10% possibility of populated cell
@@ -34,11 +82,11 @@ void init_grid(std::vector<int>& grid) {
     }
 }
 
-inline int count_neighbors(const std::vector<int>& grid, int r, int c) {
+inline int count_neighbors(const std::vector<int>& grid, int r, int c, const int SCAN_SIZE, const int ROWS, const int COLS) {
     int count = 0;
     int radius = SCAN_SIZE/2;
 
-    //to scan in a block SCAN_SIZExSCAN_SIZE around the position
+    //to scan in a block with dimension SCAN_SIZExSCAN_SIZE around the position
     for (int i = -radius; i <= radius; i++) {
         for (int j = -radius; j <= radius; j++) {
             if (i == 0 && j == 0) continue;
@@ -52,13 +100,13 @@ inline int count_neighbors(const std::vector<int>& grid, int r, int c) {
             if (nc < 0) nc = COLS - 1;
             else if (nc >= COLS) nc = 0;
 
-            count += grid[get_idx(nr, nc)];
+            count += grid[get_idx(nr, nc, COLS)];
         }
     }
     return count;
 }
 
-void update_grid(const std::vector<int>& current, std::vector<int>& next) {
+void update_grid(const std::vector<int>& current, std::vector<int>& next, const int ROWS, const int COLS, const int SCAN_SIZE) {
 
     //this is the core of the simulation because it implements the main rules:
     //1. A live cell with fewer than two live neighbors, dies (underpopulation).
@@ -69,8 +117,8 @@ void update_grid(const std::vector<int>& current, std::vector<int>& next) {
     for (int i = 0; i < ROWS; ++i) {
         for (int j = 0; j < COLS; ++j) {
 
-            int neighbors = count_neighbors(current, i, j);
-            int idx = get_idx(i, j);
+            int neighbors = count_neighbors(current, i, j, SCAN_SIZE, ROWS, COLS);
+            int idx = get_idx(i, j, COLS);
             int is_alive = current[idx];
 
             if (is_alive) {
@@ -91,6 +139,24 @@ void update_grid(const std::vector<int>& current, std::vector<int>& next) {
     }
 }
 
+void append_csv(const std::string& CSV, int SCREEN_WIDTH, int SCREEN_HEIGHT, int ROWS, int COLS, int SCAN_SIZE, int FRAMES, int THREADS, const double TIME_MS)
+{
+    static bool first = true;
+    std::ofstream out(CSV, std::ios::app);
 
+    if (first) {
+        out << "SCREEN_WIDTH, SCREEN_HEIGHT, ROWS, COLS, SCAN_SIZE, FRAMES, THREADS, time_ms\n";
+        first = false;
+    }
+
+    out << SCREEN_WIDTH << ","
+        << SCREEN_HEIGHT << ","
+        << ROWS << ","
+        << COLS << ","
+        << SCAN_SIZE << ","
+        << FRAMES << ","
+        << THREADS << ","
+        << TIME_MS << "\n";
+}
 
 #endif //PARALLEL_COMPUTING_LAB_SEQ_HEADER_H
